164fda4af538ee0317d20eda6e1c7797a7e50b65
Karl1018 <karl19991018@gmail.com>
2024-03-29 01:04:20 +0100

    fix: Mac mps compatibility

--- model/training_loop.py
+++ model/training_loop.py
@@ -9,7 +9,14 @@ from utils import transforms
 from . import network
 from . import loss
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+
 generator = network.Generator().to(device)
 discriminator = network.Discriminator().to(device)
 
