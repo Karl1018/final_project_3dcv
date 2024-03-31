c810312de6e17c0b516a49b54a8256e95aaf9764
Karl1018 <karl19991018@gmail.com>
2024-03-31 21:24:35 +0200

    remove counterproductive color augmentation

--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -1,16 +1,14 @@
 from torchvision import datasets
 from torch.utils.data import DataLoader
-from torchvision.transforms import Compose, ToTensor, Normalize, RandomHorizontalFlip, RandomRotation, ColorJitter
+from torchvision.transforms import Compose, ToTensor, Normalize, RandomHorizontalFlip, RandomRotation
 
-#TODO: Augmentations
-def make_public_dataloader(batch_size, num_workers=4, aug = False):
+def make_public_dataloader(batch_size, num_workers=4, aug=False):
     if aug:
         transform = Compose([
             RandomHorizontalFlip(),
             RandomRotation(10),
-            ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             ToTensor(),
-            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
     else:
         transform = Compose([
@@ -23,16 +21,14 @@ def make_public_dataloader(batch_size, num_workers=4, aug = False):
 
     return dataloader
 
-# custom dataloader with no labels
 def make_custom_dataloader(path, batch_size, num_workers=4, aug=False):
 
     if aug:
         transform = Compose([
             RandomHorizontalFlip(),
             RandomRotation(10),
-            ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             ToTensor(),
-            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
     else:
         transform = Compose([
